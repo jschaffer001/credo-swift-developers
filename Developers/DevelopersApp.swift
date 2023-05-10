f7d88af5897de5044a868fe862553f39430e96ce
Jonathan <7716662+jschaffer001@users.noreply.github.com>
2023-05-10 09:03:39 -0700

    Initial Commit

---
+++ Developers/DevelopersApp.swift
@@ -0,0 +1,19 @@
+// DevelopersApp.swift
+// Developers
+// 
+// Created by Jonathan Schaffer
+// Using Swift 5.0
+//
+// https://jonathanschaffer.com
+// Copyright © 2023 Jonathan Schaffer. All rights reserved.
+
+import SwiftUI
+
+@main
+struct DevelopersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
